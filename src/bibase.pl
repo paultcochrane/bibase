089f3ff06f4638c400877ab0eb549e67d1c90899
Paul Cochrane <paultcochrane@gmail.com>
2013-04-03 18:54:59 +0200

    DB_File wasn't being used; removed mention of it

--- src/bibase.pl
+++ src/bibase.pl
@@ -18,8 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
 # USA.
 
-use DB_File;
-
 $bibErrMsg = "field is required for BibTeX\n\n";
 
 if ( $^O eq "linux" || $^O eq "dec_osf" ) {
