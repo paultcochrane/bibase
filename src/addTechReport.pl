c86c0664818249ce77f78231611de97aa1e936bb
Paul Cochrane <paultcochrane@gmail.com>
2013-04-13 20:08:30 +0200

    Using warnings and strict in addTechReport.pl
    Localised and globalised variables where necessary

--- src/addTechReport.pl
+++ src/addTechReport.pl
@@ -16,63 +16,68 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
 # USA.
 
+use warnings;
+use strict;
+
 sub addTechReport {
 
-    local (
-        $bibFile,  $bibInFile, $title,    $author,   $institution,
-        $year,     $number,    $type,     $address,  $month,
-        $keywords, $dummy,     $dummyOld, $NumLines, @InArray,
-        $checkNum, $answer,    $cound,    $bibkey
-    );
-
-    open( bibFile,   ">> $DBFile" ) or die "$!";
-    open( bibInFile, "< $DBFile" )  or die "$!";
+    open( my $bibFile,   ">> $main::DBFile" ) or die "$!";
+    open( my $bibInFile, "< $main::DBFile" )  or die "$!";
     print("Choosing to add a technical report\n\n");
 
+    my $title;
     print("Title: ");
     chop( $title = <> );
 
     if ( $title eq "" ) {
-        print "Title $bibErrMsg";
+        print "Title $main::bibErrMsg";
         &add;
     }
 
+    my $author;
     print("Author(s): ");
     chop( $author = <> );
 
     if ( $author eq "" ) {
-        print "Author $bibErrMsg";
+        print "Author $main::bibErrMsg";
         &add;
     }
 
+    my $institution;
     print("Institution: ");
     chop( $institution = <> );
 
     if ( $institution eq "" ) {
-        print "Institution $bibErrMsg";
+        print "Institution $main::bibErrMsg";
         &add;
     }
 
+    my $year;
     print("Year: ");
     chop( $year = <> );
 
     if ( $year eq "" ) {
-        print "Year $bibErrMsg";
+        print "Year $main::bibErrMsg";
         &add;
     }
 
+    my $number;
     print("Number: ");
     chop( $number = <> );
 
+    my $type;
     print("Type: ");
     chop( $type = <> );
 
+    my $address;
     print("Address: ");
     chop( $address = <> );
 
+    my $month;
     print("Month: ");
     chop( $month = <> );
 
+    my $keywords;
     print("Keywords: ");
     chop( $keywords = <> );
 
@@ -81,25 +86,27 @@ sub addTechReport {
         &add;
     }
 
-    $dummy = $author;
+    my $dummy = $author;
     $dummy =~ s/\sand[\w\W]*//;
-    $dummyOld = zzzzzzzz;
+    my $dummyOld = "zzzzzzzz";
     while ( $dummyOld ne $dummy ) {
         $dummyOld = $dummy;
         $dummy =~ s/[^\s]\S*\s//;
     }
 
-    $NumLines = 0;
-    while (<bibInFile>) {
+    my $NumLines = 0;
+    my @InArray;
+    while (<$bibInFile>) {
         @InArray[$NumLines] = $_;
         $NumLines++;
     }
 
-    $checkNum = grep( /\{$title\}/, @InArray );
+    my $checkNum = grep( /\{$title\}/, @InArray );
 
     if ( $checkNum > 0 ) {
         print "This title already exists in database\n";
         print "Add anyway? (y/n) ";
+        my $answer;
         chop( $answer = <> );
         if ( $answer eq "y" ) {
         }
@@ -111,34 +118,34 @@ sub addTechReport {
         }
     }
 
-    $count = grep( /\{$dummy:$year/i, @InArray );
+    my $count = grep( /\{$dummy:$year/i, @InArray );
     $count++;
 
-    $bibkey = join( ":", $dummy, $year, $count );
+    my $bibkey = join( ":", $dummy, $year, $count );
 
-    print( bibFile "\@TechReport{$bibkey,\n" );
-    print( bibFile "author = {$author},\n" );
-    print( bibFile "title = {$title},\n" );
-    print( bibFile "institution = {$institution},\n" );
-    print( bibFile "year = {$year},\n" );
+    print( $bibFile "\@TechReport{$bibkey,\n" );
+    print( $bibFile "author = {$author},\n" );
+    print( $bibFile "title = {$title},\n" );
+    print( $bibFile "institution = {$institution},\n" );
+    print( $bibFile "year = {$year},\n" );
 
     if ( $number ne "" ) {
-        print( bibFile "number = {$number},\n" );
+        print( $bibFile "number = {$number},\n" );
     }
     if ( $type ne "" ) {
-        print( bibFile "type = {$type},\n" );
+        print( $bibFile "type = {$type},\n" );
     }
     if ( $address ne "" ) {
-        print( bibFile "address = {$address},\n" );
+        print( $bibFile "address = {$address},\n" );
     }
     if ( $month ne "" ) {
-        print( bibFile "month = {$month},\n" );
+        print( $bibFile "month = {$month},\n" );
     }
-    print( bibFile "keywords = {$keywords}\n" );
-    print( bibFile "}\n\n" );
+    print( $bibFile "keywords = {$keywords}\n" );
+    print( $bibFile "}\n\n" );
 
-    close(bibFile);
-    close(bibInFile);
+    close($bibFile);
+    close($bibInFile);
 
     # @TechReport{,
     #   author =      {},
