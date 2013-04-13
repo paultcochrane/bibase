601751867484a2eef1492c587851b0a7cf856265
Paul Cochrane <paultcochrane@gmail.com>
2013-04-13 11:43:34 +0200

    Using warnings and strict in addManual.pl
    Localised and globalised variables where necessary

--- src/addManual.pl
+++ src/addManual.pl
@@ -16,55 +16,59 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
 # USA.
 
+use warnings;
+use strict;
+
 sub addManual {
 
-    local (
-        $bibFile, $bibInFile, $title,    $author,  $organisation,
-        $year,    $address,   $edition,  $month,   $keywords,
-        $dummy,   $dummyOld,  $NumLines, @InArray, $checkNum,
-        $answer,  $count,     $bibkey
-    );
-
-    open( bibFile,   ">> $DBFile" ) or die "$!";
-    open( bibInFile, "< $DBFile" )  or die "$!";
+    open( my $bibFile,   ">> $main::DBFile" ) or die "$!";
+    open( my $bibInFile, "< $main::DBFile" )  or die "$!";
     print("Choosing to add a manual\n\n");
 
+    my $title;
     print("Title: ");
     chop( $title = <> );
 
     if ( $title eq "" ) {
-        print "Title $bibErrMsg";
+        print "Title $main::bibErrMsg";
         &add;
     }
 
+    my $author;
     print("Author(s): ");
     chop( $author = <> );
 
     if ( $author eq "" ) {
-        print "Author $bibErrMsg";
+        print "Author $main::bibErrMsg";
         &add;
     }
 
+    my $organisation;
     print("Organisation: ");
     chop( $organisation = <> );
 
+    my $year;
     print("Year: ");
     chop( $year = <> );
 
     if ( $year eq "" ) {
-        print "Year $bibErrMsg";
+        print "Year $main::bibErrMsg";
         &add;
     }
 
+    my $address;
     print("Address: ");
     chop( $address = <> );
 
+    my $edition;
     print("Edition: ");
     chop( $edition = <> );
 
+    my $month;
     print("Month: ");
     chop( $month = <> );
 
+    my $keywords;
     print("Keywords: ");
     chop( $keywords = <> );
 
@@ -73,25 +77,27 @@ sub addManual {
         &add;
     }
 
-    $dummy = $author;
+    my $dummy = $author;
     $dummy =~ s/\sand[\w\W]*//;
-    $dummyOld = zzzzzzzz;
+    my $dummyOld = "zzzzzzzz";
     while ( $dummyOld ne $dummy ) {
         $dummyOld = $dummy;
         $dummy =~ s/[^\s]\S*\s//;
     }
 
-    $NumLines = 0;
-    while (<bibInFile>) {
+    my $NumLines = 0;
+    my @InArray;
+    while (<$bibInFile>) {
         @InArray[$NumLines] = $_;
         $NumLines++;
     }
 
-    $checkNum = grep( /\{$title\}/, @InArray );
+    my $checkNum = grep( /\{$title\}/, @InArray );
 
     if ( $checkNum > 0 ) {
         print "This title already exists in database\n";
         print "Add anyway? (y/n) ";
+        my $answer;
         chop( $answer = <> );
         if ( $answer eq "y" ) {
         }
@@ -103,31 +109,31 @@ sub addManual {
         }
     }
 
-    $count = grep( /\{$dummy:$year/i, @InArray );
+    my $count = grep( /\{$dummy:$year/i, @InArray );
     $count++;
 
-    $bibkey = join( ":", $dummy, $year, $count );
+    my $bibkey = join( ":", $dummy, $year, $count );
 
-    print( bibFile "\@Manual{$bibkey,\n" );
-    print( bibFile "author = {$author},\n" );
-    print( bibFile "title = {$title},\n" );
-    print( bibFile "organization = {$organisation},\n" );
-    print( bibFile "year = {$year},\n" );
+    print( $bibFile "\@Manual{$bibkey,\n" );
+    print( $bibFile "author = {$author},\n" );
+    print( $bibFile "title = {$title},\n" );
+    print( $bibFile "organization = {$organisation},\n" );
+    print( $bibFile "year = {$year},\n" );
 
     if ( $address ne "" ) {
-        print( bibFile "address = {$address},\n" );
+        print( $bibFile "address = {$address},\n" );
     }
     if ( $edition ne "" ) {
-        print( bibFile "edition = {$edition},\n" );
+        print( $bibFile "edition = {$edition},\n" );
     }
     if ( $month ne "" ) {
-        print( bibFile "month = {$month},\n" );
+        print( $bibFile "month = {$month},\n" );
     }
-    print( bibFile "keywords = {$keywords}\n" );
-    print( bibFile "}\n\n" );
+    print( $bibFile "keywords = {$keywords}\n" );
+    print( $bibFile "}\n\n" );
 
-    close(bibFile);
-    close(bibInFile);
+    close($bibFile);
+    close($bibInFile);
 
     # @Manual{,
     #   title =      {},
