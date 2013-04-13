69297004da854db67271564f601fe311f6f87639
Paul Cochrane <paultcochrane@gmail.com>
2013-04-13 20:34:31 +0200

    Using warnings and strict in bibkeyMake.pl

--- src/bibkeyMake.pl
+++ src/bibkeyMake.pl
@@ -16,29 +16,31 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
 # USA.
 
+use warnings;
+use strict;
+
 sub bibkeyMake {
 
-    local ( $dummy, $dummyOld, $NumLines, @InArray, $count );
-
-    $dummy = $author;
+    my $dummy = $main::author;
     $dummy =~ s/\sand[\w\W]*//;    # deletes everything after first 'and'
-    $dummyOld = zzzzzzzz;
+    my $dummyOld = "zzzzzzzz";
     while ( $dummyOld ne $dummy ) {
         $dummyOld = $dummy;
         $dummy =~
           s/[^\s]\S*\s//;    # deletes all characters in front of first surname
     }
 
-    $NumLines = 0;
-    while (<bibInFile>) {
-        @InArray[$NumLines] = $_;
+    my $NumLines = 0;
+    my @InArray;
+    while (<$main::bibInFile>) {
+        $InArray[$NumLines] = $_;
         $NumLines++;
     }
 
-    $count = grep( /\{$dummy:$year/i, @InArray );
+    my $count = grep( /\{$dummy:$main::year/i, @InArray );
     $count++;
 
-    $bibkey = join( ":", $dummy, $year, $count );
+    my $bibkey = join( ":", $dummy, $main::year, $count );
 
 }
 1;
